b08e5d9542fa3354d3cd9626398e0e1213e65933
Shubham Kulkarni <shubhamkulkarni01@gmail.com>
2020-04-18 18:57:23 -0700

    fully dockerized, use ./build.sh to rebuild and create deployment-ready containers

--- build.sh
+++ build.sh
@@ -1,2 +1,20 @@
-npm run --prefix client/ build
+echo "Cleaning out the old builds"
+# clean out the old build folder
+rm -rf client/build nginx/build
+
+echo "Creating a new production build"
+# create a production react build
+npm run --silent --prefix client/ build > /dev/null
+# set up production react build to be served by nginx
 cp -r client/build nginx/
+
+echo "Rebuilding containers with new production build"
+# build the containers
+docker-compose build --quiet
+
+echo "Start the system now? Y/n (default is yes)"
+read -n 1 answer
+if [ "$answer" != "n" ]
+then
+    docker-compose up
+fi
